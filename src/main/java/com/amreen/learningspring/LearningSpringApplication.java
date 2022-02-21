7eca68f847c6a50d9af5a498affc174627ff76b3
Amreen Shaikh <amreen.rajmohmad@khoros.com>
2022-02-21 15:49:06 +0530

    data access with spring repositories

---
+++ src/main/java/com/amreen/learningspring/LearningSpringApplication.java
@@ -0,0 +1,31 @@
+package com.amreen.learningspring;
+
+import com.amreen.learningspring.data.entity.Room;
+import com.amreen.learningspring.data.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class LearningSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringApplication.class, args);
+	}
+
+	@RequestMapping("/rooms")
+	@RestController
+	public class RoomController{
+		@Autowired
+		private RoomRepository roomRepository;
+
+		@GetMapping
+		public Iterable<Room> getRooms() {
+			return this.roomRepository.findAll();
+		}
+	}
+
+}
